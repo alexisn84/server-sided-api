fcf985e93e98e9c665e5d91f10a5c8a9aa826dbd
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2021-11-15 11:02:04 -0500

    build city search input form and button

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+//global variables
+
+
+//city entered
+
+
+//pull city list fromlocal storage
+
+
+//pulls city into local storage and reloads
+
+
+//function to save city (maybe as an array) into local storage
+
+
+//function to save current display into local storage
+
+
+//click event for city search button
+
+
+//event handler for "enter" instead of click
+
+//function to run api aja call and display current city, weather and 5 day forecaset to the dom, dont forget uv index
+
+
+//function to call the 5 fday and display
+
+// funtion to make history list
+
+
+
+
