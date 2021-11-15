8a1d7e6e87edf06652f2e3459285db85328dd829
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2021-11-15 17:12:41 -0500

    beginning of js file, got city input field to work and put in history and in storage

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,80 @@
 //global variables
-
+var cityList = [];
+var cityname;
 
 //city entered
+var showCity = function() {
+    $("#cityList").empty();
+    $("#cityInput").val("");
 
+    //loop to add city to history list
+    for (i=0; i < cityList.length; i++) {
+        //test out jquery to add elements to hold searched cities 
+        var searchedCity = $("<a>");
+        searchedCity.addClass("list-group-item list-group-item-action list-group-item-primary city");
+        searchedCity.attr("data-name", cityList[i]);
+        searchedCity.text(cityList[i]);
+        $("#cityList").prepend(searchedCity);
+        
+    }
+}
 
 //pull city list fromlocal storage
+var pullCityList = function() {
+    var fromLocalStorage = JSON.parse(localStorage.getItem("cities"));
 
+    if (fromLocalStorage !== null) {
+        cityList = fromLocalStorage;
+    }
+
+    //call showCity
+    showCity();
+}
 
 //pulls city into local storage and reloads
+var showWeather = function() {
+    var storedCity = JSON.parse(localStorage.getItem("currentCity"));
 
+    if (storedCity !== null) {
+        cityname = storedCity;
+
+        displayWeather();
+        displayFiveDayForecast();
+    }
+}
 
 //function to save city (maybe as an array) into local storage
-
+var storeCityArray = function() {
+    localStorage.setItem("cities", JSON.stringify(cityList));
+}
 
 //function to save current display into local storage
-
+var storeCurrentCity = function() {
+    localStorage.setItem("currentCity", JSON.stringify(cityname));
+}
 
 //click event for city search button
+$("#citySearch").on("click", function(event) {
+    event.preventDefault();
+
+    cityname = $("#cityInput").val().trim();
+    if (cityname === "") {
+
+    } else if (cityList.length >= 5) {
+        cityList.shift();
+        cityList.push(cityname);
+
+    } else {
+        cityList.push(cityname);
+    }
+
+    //call functions
+    storeCurrentCity();
+    storeCityArray();
+    showCity();
+    //displayWeather();
+    //displayFiveDayForecast();
+});
 
 
 //event handler for "enter" instead of click
@@ -28,6 +86,8 @@
 
 // funtion to make history list
 
-
+// local storage functions
+// pullCityList();
+// showWeather();
 
 
