17ac66b703d77affe4e908bc081e2ee6bcba7893
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2021-11-17 12:10:34 -0500

    addition of weather details call and response, glitch in color coding index

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,26 @@
 var cityList = [];
 var cityname;
 
+//global mainly used to make connection to api one weather
+var currentCity = $("#searched-city");
+var currentTemp = $("#current-temp");
+var currentHumidity = $("#current-humidity");
+var currentWindSpeed = $("#current-wind-speed");
+var UVindex = $("#uv-index");
+
+//global variables mainly with weather display
+var weatherContent = $("#weather-container");
+
+//access to the OpenWeather API
+var APIkey = "116ba6c8059f9b9fbbd135332921be2e";
+
+// show date
+var currentDate = moment().format('L');
+$("#todaysDate").text("(" + currentDate + ")");
+
+//jquery to color date
+//$(todaysDate").css("color", "#e1aa7d");
+
 //city entered
 var showCity = function() {
     $("#cityList").empty();
@@ -14,9 +34,12 @@ var showCity = function() {
         searchedCity.addClass("list-group-item list-group-item-action list-group-item-primary city");
         searchedCity.attr("data-name", cityList[i]);
         searchedCity.text(cityList[i]);
+        
         $("#cityList").prepend(searchedCity);
         
     }
+    //use jquery to change the background color of the item populated
+    $("a").css("background-color", "#6a2e35");
 }
 
 //pull city list fromlocal storage
@@ -72,19 +95,79 @@ $("#citySearch").on("click", function(event) {
     storeCurrentCity();
     storeCityArray();
     showCity();
-    //displayWeather();
+    showWeather();
     //displayFiveDayForecast();
 });
 
-
-//event handler for "enter" instead of click
-
 //function to run api aja call and display current city, weather and 5 day forecaset to the dom, dont forget uv index
+var showWeather = function() {
+    //url for ajax api call
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=" + APIkey;
 
+    //ajax call/
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function(response){
+        console.log(response);
+
+        currentCity.text(response.name);
+        currentCity.append("<small class='text-muted' id='todaysDate'>");
+        $("#todaysDate").text("(" + currentDate + ")");
+        currentCity.append("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='" + response.weather[0].main + "' />" )
+        currentTemp.text(response.main.temp);
+        currentTemp.append("&deg;F");
+        currentHumidity.text(response.main.humidity + "%");
+        currentWindSpeed.text(response.wind.speed + "MPH");
+
+        //get UV index
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+
+        var UVurl = "https://api.openweathermap.org/data/2.5/uvi?&lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+        
+        var UVresult = $.ajax({
+            url: UVurl,
+            method: "GET"
+        }).then(function(response){
+            // console.log("UV call: ")
+            // console.log(response);
+            UVindex.text(response.value);            
+        });
+
+        //colorcode uv index
+        var UVindex = UVresult.value;
+        var UVspan = $("uvSpot");
+
+        if (UVindex > 0 && UVIndex <= 2.99) {
+            UVspan.addClass("low");
+        }
+        else if (UVindex >= 3 && UVindex <= 5.99) {
+            UVspan.addClass("moderate");
+        }
+        else {UVspan.addClass("high");
+        }
+        
+
+        //get 5 day forecast
+        var countryCode = response.sys.country;
+        var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=" + APIkey + "&lat=" + lat +  "&lon=" + lon;
+
+
+    });
+};
 
 //function to call the 5 fday and display
 
 // funtion to make history list
+
+//check if enter key hit instead of click
+$("#cityInput").keypress(function(event) {
+    if (event.which === 13) {
+        $("#citySearch").click();
+    }
+})
 
 // local storage functions
 // pullCityList();
